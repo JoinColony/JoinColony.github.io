7534f96b179c57c573b260229f44b2305b5d0e70
Curtis Olson <curtis@colony.io>
2019-01-29 11:59:02 -0600

    Fix gatsby external links warnings
    * Reverse `internal` vs `external` logic
    * Use standard `a` element for on-page links
    * Only apply `target` & `rel` if off-page, external links
    * Type the extra props so they are not too magical

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -4,27 +4,49 @@ import type { Node } from 'react';
 import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
 
-function isExternal(url) {
-  if ((url && url.startsWith('http')) || url.startsWith('mailto')) {
-    return true;
-  }
-  return false;
-}
+type ExtraUrlProps = {|
+  target?: '_blank',
+  rel?: 'noopener noreferrer',
+|};
 
 type Props = {
   children: Node,
   href: string,
 };
 
+/*
+ * A url is only considered to be "internal" if it starts with one slash.
+ */
+const isInternal = (url: string): boolean => /^\/(?!\/)/.test(url);
+
+/*
+ * On-page anchors are considered "external" by Gatsby.
+ */
+const isOnPageAnchor = (url: string): boolean => /^#(?!\/)/.test(url);
+
+/*
+ * If the anchor is an on-page-anchor (i.e. #get-started),
+ * gatsby warns not to use the `Link` component, as it
+ * refers to these as tehcnically being external links.
+ */
+const getUrlProps = (url: string): ExtraUrlProps | void => {
+  if (!isOnPageAnchor(url))
+    return {
+      target: '_blank',
+      rel: 'noopener noreferrer',
+    };
+  return undefined;
+};
+
 const Link = ({ children, href, ...props }: Props) =>
-  isExternal(href) ? (
-    <a {...props} href={href} target="_blank" rel="noopener noreferrer">
-      {children}
-    </a>
-  ) : (
+  isInternal(href) ? (
     <GatsbyLink to={href} {...props}>
       {children}
     </GatsbyLink>
+  ) : (
+    <a {...props} href={href} {...getUrlProps(href)}>
+      {children}
+    </a>
   );
 
 export default Link;
