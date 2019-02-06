7a6429d545a674a41ee66bd77599476cf9a5fa2e
Curtis Olson <curtis@colony.io>
2019-02-05 18:35:51 -0600

    Implement `react-intl` for `Image`

--- src/modules/core/components/Image/Image.jsx
+++ src/modules/core/components/Image/Image.jsx
@@ -1,30 +1,47 @@
 /* @flow */
+import type { IntlShape, MessageDescriptor } from 'react-intl';
+
 import React from 'react';
+import { injectIntl } from 'react-intl';
 
 import FileContext from '~context/FileContext';
 
 type Props = {
-  alt: string,
-  src: string,
+  alt: MessageDescriptor | string,
+  altValues?: Object,
+  intl: IntlShape,
   project: string,
+  src: string,
 };
 
 const displayName = 'Image';
 
-const Image = ({ alt, src, project, ...props }: Props) => (
-  <FileContext.Consumer>
-    {files => (
-      <img
-        {...props}
-        alt={alt}
-        src={
-          files && files[`${project}/${src}`] ? files[`${project}/${src}`] : src
-        }
-      />
-    )}
-  </FileContext.Consumer>
-);
+const Image = ({
+  alt,
+  altValues,
+  intl: { formatMessage },
+  project,
+  src,
+  ...props
+}: Props) => {
+  const altText = typeof alt === 'string' ? alt : formatMessage(alt, altValues);
+  return (
+    <FileContext.Consumer>
+      {files => (
+        <img
+          {...props}
+          alt={altText}
+          src={
+            files && files[`${project}/${src}`]
+              ? files[`${project}/${src}`]
+              : src
+          }
+        />
+      )}
+    </FileContext.Consumer>
+  );
+};
 
 Image.displayName = displayName;
 
-export default Image;
+export default injectIntl(Image);
