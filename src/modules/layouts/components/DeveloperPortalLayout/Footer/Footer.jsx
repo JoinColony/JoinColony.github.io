0d4e88e85170efb43164f127cd39d66bd74c5475
Curtis Olson <curtis@colony.io>
2019-03-07 00:37:21 -0600

    Add developer portal `Footer` component

---
+++ src/modules/layouts/components/DeveloperPortalLayout/Footer/Footer.jsx
@@ -0,0 +1,86 @@
+/* @flow */
+import React from 'react';
+import { defineMessages } from 'react-intl';
+
+import VerticalMenu from './VerticalMenu';
+
+import styles from './Footer.module.css';
+
+const MSG = defineMessages({
+  navTitleGetStarted: {
+    id: 'layouts.DeveloperPortalLayout.Footer.navTitleGetStarted',
+    defaultMessage: 'Get Started',
+  },
+  navLinkTutorials: {
+    id: 'layouts.DeveloperPortalLayout.Footer.navLinkTutorials',
+    defaultMessage: 'Tutorials',
+  },
+  navLinkSupport: {
+    id: 'layouts.DeveloperPortalLayout.Footer.navLinkSupport',
+    defaultMessage: 'Support',
+  },
+  navTitleCoreProducts: {
+    id: 'layouts.DeveloperPortalLayout.Footer.navTitleCoreProducts',
+    defaultMessage: 'Colony Core',
+  },
+  navTitleOpenSourceTools: {
+    id: 'layouts.DeveloperPortalLayout.Footer.navTitleCoreProducts',
+    defaultMessage: 'Colony Core',
+  },
+});
+
+const displayName = 'layouts.DeveloperPortalLayout.Footer';
+
+const Footer = () => (
+  <div className={styles.main}>
+    <div className={styles.navRow}>
+      <div className={styles.logoContainer}>{/* @TODO: logo here */}</div>
+      <div className={styles.navGroup}>
+        <div className={styles.navGroupItem}>
+          <VerticalMenu
+            headingText={MSG.navTitleGetStarted}
+            menuItems={[
+              { href: '/tutorials', text: MSG.navLinkTutorials },
+              { href: '/support', text: MSG.navLinkSupport },
+            ]}
+          />
+        </div>
+        <div className={styles.navGroupItem}>
+          <VerticalMenu
+            headingText={MSG.navTitleCoreProducts}
+            menuItems={[
+              // @TODO fix these links...
+              { href: '/network', text: 'colonyNetwork' },
+              { href: '/starter', text: 'colonyStarter' },
+              { href: '/js', text: 'colonyJS' },
+            ]}
+          />
+        </div>
+        <div className={styles.navGroupItem}>
+          <VerticalMenu
+            headingText={MSG.navTitleOpenSourceTools}
+            menuItems={[
+              // @TODO fix these links...
+              { href: '/budgetbox', text: 'budgetBox' },
+              { href: '/pinion', text: 'pinion' },
+              { href: '/purser', text: 'purser' },
+              { href: '/solidity-coverage', text: 'solidity-coverage' },
+              { href: '/tailor', text: 'tailor' },
+              { href: '/trufflepig', text: 'trufflepig' },
+            ]}
+          />
+        </div>
+      </div>
+    </div>
+    <div className={styles.metaRow}>
+      <div className={styles.metaNavContainer}>
+        {/* @TODO: nav items here */}
+      </div>
+      <div className={styles.iconContainer}>{/* @TODO: icon list here */}</div>
+    </div>
+  </div>
+);
+
+Footer.displayName = displayName;
+
+export default Footer;
