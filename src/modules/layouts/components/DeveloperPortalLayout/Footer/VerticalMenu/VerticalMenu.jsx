4131c0b93d6c7b65e2d4de7186535d2b7f834c3d
Curtis Olson <curtis@colony.io>
2019-03-07 00:38:42 -0600

    Add `VerticalMenu` component for footer menus

---
+++ src/modules/layouts/components/DeveloperPortalLayout/Footer/VerticalMenu/VerticalMenu.jsx
@@ -0,0 +1,38 @@
+/* @flow */
+import type { MessageDescriptor } from 'react-intl';
+
+import React from 'react';
+
+import type { InProps as LinkProps } from '~core/Link/types';
+
+import Heading from '~core/Heading';
+import Link from '~core/Link';
+
+type Props = {|
+  headingText?: MessageDescriptor | string,
+  headingTextValues?: Object,
+  menuItems: Array<LinkProps>,
+|};
+
+const displayName = 'layouts.DeveloperPortalLayout.Footer.VerticalMenu';
+
+const VerticalMenu = ({ headingText, headingTextValues, menuItems }: Props) => {
+  return (
+    <div>
+      {headingText && (
+        <Heading text={headingText} textValues={headingTextValues} />
+      )}
+      {menuItems.length > 0 && (
+        <nav>
+          {menuItems.map(menuItemProps => (
+            <Link {...menuItemProps} />
+          ))}
+        </nav>
+      )}
+    </div>
+  );
+};
+
+VerticalMenu.displayName = displayName;
+
+export default VerticalMenu;
