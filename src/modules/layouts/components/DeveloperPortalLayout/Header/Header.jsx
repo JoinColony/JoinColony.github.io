747dfdba35b3c65e005943fb5a9be2d36715e218
Curtis Olson <curtis@colony.io>
2019-03-07 22:47:53 -0600

    Add `Popover` to `Header`

--- src/modules/layouts/components/DeveloperPortalLayout/Header/Header.jsx
+++ src/modules/layouts/components/DeveloperPortalLayout/Header/Header.jsx
@@ -1,9 +1,10 @@
 /* @flow */
-import React from 'react';
+import React, { Component } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
 
 import Icon from '~core/Icon';
 import Link from '~core/Link';
+import Popover from '~core/Popover';
 
 import styles from './Header.module.css';
 
@@ -26,46 +27,87 @@ const MSG = defineMessages({
   },
 });
 
+type Props = {};
+
+type State = {
+  isDocsOpen: boolean,
+};
+
 const displayName = 'layouts.DeveloperPortalLayout.Header';
 
-const Header = () => (
-  <div className={styles.main}>
-    <div className={styles.menuWrapper}>
-      <Link href="/developers">
-        <Icon
-          className={styles.logo}
-          name="developerPortal_white"
-          title={MSG.imageAltDevPortal}
-          viewBox="0 0 134 33"
-        />
-      </Link>
-      <div className={styles.navContainer}>
-        <nav
-          className={styles.navigation}
-          role="navigation"
-          aria-label="Main Navigation"
-        >
-          <Link className={styles.navLink} href="/docs">
-            <FormattedMessage {...MSG.navLinkDocs} />
-            {/* @TODO: dropdown nav here */}
+class Header extends Component<Props, State> {
+  state = {
+    isDocsOpen: false,
+  };
+
+  closeDocsDropdown = () => {
+    this.setState({
+      isDocsOpen: false,
+    });
+  };
+
+  openDocsDropdown = () => {
+    this.setState({
+      isDocsOpen: true,
+    });
+  };
+
+  render() {
+    const { isDocsOpen } = this.state;
+    return (
+      <div className={styles.main}>
+        <div className={styles.menuWrapper}>
+          <Link href="/developers">
+            <Icon
+              className={styles.logo}
+              name="developerPortal_white"
+              title={MSG.imageAltDevPortal}
+              viewBox="0 0 134 33"
+            />
           </Link>
-          <Link className={styles.navLink} href="/tutorials">
-            <FormattedMessage {...MSG.navLinkTutorials} />
-            {/* @TODO: dropdown nav here */}
-          </Link>
-          <Link
-            className={styles.navLink}
-            href="/support"
-            text={MSG.navLinkSupport}
-          />
-        </nav>
-        <div className={styles.searchContainer}>
-          {/* @TODO: search component here */}
+          <div className={styles.navContainer}>
+            <nav
+              className={styles.navigation}
+              role="navigation"
+              aria-label="Main Navigation"
+            >
+              <span
+                className={styles.navLink}
+                onMouseLeave={this.closeDocsDropdown}
+              >
+                <Popover
+                  content={() => <span>Render prop content</span>}
+                  isOpen={isDocsOpen}
+                  placement="bottom"
+                  trigger="disabled"
+                >
+                  <div
+                    className={styles.dropdownParent}
+                    onMouseEnter={this.openDocsDropdown}
+                  >
+                    <FormattedMessage {...MSG.navLinkDocs} />
+                  </div>
+                </Popover>
+              </span>
+              <Link className={styles.navLink} href="/tutorials">
+                <FormattedMessage {...MSG.navLinkTutorials} />
+                {/* @TODO: dropdown nav here */}
+              </Link>
+              <Link
+                className={styles.navLink}
+                href="/support"
+                text={MSG.navLinkSupport}
+              />
+            </nav>
+            <div className={styles.searchContainer}>
+              {/* @TODO: search component here */}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 Header.displayName = displayName;
 
