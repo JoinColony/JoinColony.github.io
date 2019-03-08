318323333dbd85fc16913a6195516d032e167410
Curtis Olson <curtis@colony.io>
2019-03-07 22:47:18 -0600

    Add `Popover` from dApp
    * With minor adjustments to flow types

---
+++ src/modules/core/components/Popover/PopoverWrapper.jsx
@@ -0,0 +1,68 @@
+/* @flow */
+
+import type { Node } from 'react';
+import type { PopperArrowProps } from 'react-popper';
+import React from 'react';
+
+// eslint-disable-next-line import/no-cycle
+import type { Appearance as PopoverAppearance, Placement } from './Popover.jsx';
+
+import { getMainClasses } from '~utils/css';
+import getPopoverArrowClasses from './getPopoverArrowClasses';
+
+import styles from './PopoverWrapper.module.css';
+
+type Appearance = PopoverAppearance & {
+  placement?: Placement,
+};
+
+type ArrowProps = PopperArrowProps & {
+  showArrow: boolean,
+};
+
+type Props = {|
+  appearance?: Appearance,
+  arrowProps: ArrowProps,
+  children: Node,
+  id: string,
+  innerRef: (?HTMLElement) => void,
+  onFocus: (evt: SyntheticFocusEvent<HTMLElement>) => void,
+  retainRefFocus?: boolean,
+  placement: string,
+  style: any,
+|};
+
+const PopoverWrapper = ({
+  appearance,
+  arrowProps,
+  children,
+  id,
+  innerRef,
+  onFocus,
+  placement,
+  retainRefFocus,
+  style,
+}: Props) => (
+  <div
+    className={getMainClasses(appearance, styles, {
+      hideArrow: !arrowProps.showArrow,
+      showArrow: arrowProps.showArrow,
+    })}
+    id={id}
+    role="tooltip"
+    ref={innerRef}
+    style={style}
+    data-placement={placement}
+    tabIndex={retainRefFocus ? '-1' : null}
+    onFocus={onFocus}
+  >
+    {children}
+    <span
+      className={getPopoverArrowClasses(appearance, placement, styles)}
+      ref={arrowProps.ref}
+      style={arrowProps.style}
+    />
+  </div>
+);
+
+export default PopoverWrapper;
