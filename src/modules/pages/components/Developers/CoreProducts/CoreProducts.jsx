cf0521cceb09687ecdd423e0060d199772eed48c
Curtis Olson <curtis@colony.io>
2019-03-20 16:02:24 -0500

    Add intro content for `CoreProducts`

--- src/modules/pages/components/Developers/CoreProducts/CoreProducts.jsx
+++ src/modules/pages/components/Developers/CoreProducts/CoreProducts.jsx
@@ -1,9 +1,38 @@
 /* @flow */
 import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+import Heading from '~core/Heading';
+
+import styles from './CoreProducts.module.css';
+
+const MSG = defineMessages({
+  sectionTitle: {
+    id: 'pages.Developers.CoreProducts.sectionTitle',
+    defaultMessage: 'Colony Core',
+  },
+  sectionText: {
+    id: 'pages.Developers.CoreProducts.sectionText',
+    defaultMessage:
+      // eslint-disable-next-line max-len
+      'Engage with Colony to manage work, shared funds, and reputation. Integrate directly with the smart contracts, use colonyJS to build Colony into your dapp, or fire up the colonyStarter for boilerplates and implementation examples.',
+  },
+});
 
 const displayName = 'pages.Developers.CoreProducts';
 
-const CoreProducts = () => <div />;
+const CoreProducts = () => (
+  <div className={styles.main}>
+    <div className={styles.gradientWrapper}>
+      <div className={styles.sectionIntroContent}>
+        <Heading appearance={{ theme: 'invert' }} text={MSG.sectionTitle} />
+        <p>
+          <FormattedMessage {...MSG.sectionText} />
+        </p>
+      </div>
+    </div>
+  </div>
+);
 
 CoreProducts.displayName = displayName;
 
