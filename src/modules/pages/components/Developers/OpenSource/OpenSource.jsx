30467b470f27c979e70678e1713fbed65f97c4a9
Curtis Olson <curtis@colony.io>
2019-03-20 19:30:37 -0500

    Add `OpenSource` section component

---
+++ src/modules/pages/components/Developers/OpenSource/OpenSource.jsx
@@ -0,0 +1,10 @@
+/* @flow */
+import React from 'react';
+
+const displayName = 'pages.Developers.OpenSource';
+
+const OpenSource = () => <div />;
+
+OpenSource.displayName = displayName;
+
+export default OpenSource;
---
+++ src/modules/pages/components/Developers/OpenSource/index.js
@@ -0,0 +1,3 @@
+/* @flow */
+
+export { default } from './OpenSource.jsx';
