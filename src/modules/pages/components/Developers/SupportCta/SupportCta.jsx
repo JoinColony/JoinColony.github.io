500aa193db50a5daf9ad5762bb95e05075801e33
Curtis Olson <curtis@colony.io>
2019-03-20 20:17:00 -0500

    Add heading

--- src/modules/pages/components/Developers/SupportCta/SupportCta.jsx
+++ src/modules/pages/components/Developers/SupportCta/SupportCta.jsx
@@ -1,9 +1,28 @@
 /* @flow */
 import React from 'react';
+import { defineMessages } from 'react-intl';
+
+import Heading from '~core/Heading';
+
+import styles from './SupportCta.module.css';
+
+const MSG = defineMessages({
+  sectionTitle: {
+    id: 'pages.Developers.SupportCta.sectionTitle',
+    defaultMessage: 'Questions? Problems? Existential dilemmas? We can help!',
+  },
+});
 
 const displayName = 'pages.Developers.SupportCta';
 
-const SupportCta = () => <div />;
+const SupportCta = () => (
+  <div className={styles.main}>
+    <Heading
+      appearance={{ size: 'huge', theme: 'primary', weight: 'medium' }}
+      text={MSG.sectionTitle}
+    />
+  </div>
+);
 
 SupportCta.displayName = displayName;
 
