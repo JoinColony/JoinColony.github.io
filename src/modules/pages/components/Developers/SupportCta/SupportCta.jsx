6c1e4927a45a886fdf8fe08e07ffa3ee2fc55145
Curtis Olson <curtis@colony.io>
2019-03-20 20:09:27 -0500

    Add skeleton support cta

---
+++ src/modules/pages/components/Developers/SupportCta/SupportCta.jsx
@@ -0,0 +1,10 @@
+/* @flow */
+import React from 'react';
+
+const displayName = 'pages.Developers.SupportCta';
+
+const SupportCta = () => <div />;
+
+SupportCta.displayName = displayName;
+
+export default SupportCta;
