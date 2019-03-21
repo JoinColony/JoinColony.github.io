bd2ff5567c928074b5e9e7afbb16503ce9312050
Curtis Olson <curtis@colony.io>
2019-03-20 19:42:15 -0500

    Add `OpenSourceItem` component

---
+++ src/modules/pages/components/Developers/OpenSourceItem/OpenSourceItem.jsx
@@ -0,0 +1,10 @@
+/* @flow */
+import React from 'react';
+
+const displayName = 'pages.Developers.OpenSourceItem';
+
+const OpenSourceItem = () => <div />;
+
+OpenSourceItem.displayName = displayName;
+
+export default OpenSourceItem;
