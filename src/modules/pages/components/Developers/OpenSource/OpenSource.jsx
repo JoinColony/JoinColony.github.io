0c9a281f6248432d016df424d3be4b1732495cda
Curtis Olson <curtis@colony.io>
2019-03-20 20:07:25 -0500

    Improve open source grid items

--- src/modules/pages/components/Developers/OpenSource/OpenSource.jsx
+++ src/modules/pages/components/Developers/OpenSource/OpenSource.jsx
@@ -4,6 +4,8 @@ import { defineMessages, FormattedMessage } from 'react-intl';
 
 import Heading from '~core/Heading';
 
+import OpenSourceItem from '../OpenSourceItem';
+
 import styles from './OpenSource.module.css';
 
 const MSG = defineMessages({
@@ -14,6 +16,41 @@ const MSG = defineMessages({
   sectionIntroText: {
     id: 'pages.Developers.OpenSource.sectionIntroText',
     defaultMessage: 'Standalone tools for Ethereum developers.',
+  },
+  descriptionBudgetBox: {
+    id: 'pages.Developers.OpenSource.descriptionBudgetBox',
+    defaultMessage:
+      // eslint-disable-next-line max-len
+      'Solidity reference implementation for a decentralized capital allocation mechanism.',
+  },
+  descriptionTailor: {
+    id: 'pages.Developers.OpenSource.descriptionTailor',
+    defaultMessage:
+      // eslint-disable-next-line max-len
+      'A universal javascript library for interacting with Ethereum: it stitches dApps to Web3.',
+  },
+  descriptionPinion: {
+    id: 'pages.Developers.OpenSource.descriptionPinion',
+    defaultMessage:
+      // eslint-disable-next-line max-len
+      `Dutifully keeps IPFS files 'pinned' and available for your decentralized application.`,
+  },
+  descriptionTrufflepig: {
+    id: 'pages.Developers.OpenSource.descriptionTrufflepig',
+    defaultMessage:
+      // eslint-disable-next-line max-len
+      'A friendly companion for devs that serves contract ABIs over http:// for local development.',
+  },
+  descriptionPurser: {
+    id: 'pages.Developers.OpenSource.descriptionPurser',
+    defaultMessage:
+      // eslint-disable-next-line max-len
+      'A universal javascript library that simplifies interaction with Ethereum wallets.',
+  },
+  descriptionSolidityCoverage: {
+    id: 'pages.Developers.OpenSource.descriptionSolidityCoverage',
+    defaultMessage:
+      'The original (TM) solidity code-coverage utility for testing.',
   },
 });
 
@@ -29,6 +66,62 @@ const OpenSource = () => (
       <p>
         <FormattedMessage {...MSG.sectionIntroText} />
       </p>
+    </div>
+    <div className={styles.openSourceProductGrid}>
+      <div className={styles.openSourceItem}>
+        {/* @TODO fix links */}
+        <OpenSourceItem
+          docsUrl="/docs/asdlkfj"
+          githubUrl="/another/thing"
+          headingText="budgetBox"
+          textContent={MSG.descriptionBudgetBox}
+        />
+      </div>
+      <div className={styles.openSourceItem}>
+        {/* @TODO fix links */}
+        <OpenSourceItem
+          docsUrl="/docs/asdlkfj"
+          githubUrl="/another/thing"
+          headingText="tailor"
+          textContent={MSG.descriptionTailor}
+        />
+      </div>
+      <div className={styles.openSourceItem}>
+        {/* @TODO fix links */}
+        <OpenSourceItem
+          docsUrl="/docs/asdlkfj"
+          githubUrl="/another/thing"
+          headingText="pinion"
+          textContent={MSG.descriptionPinion}
+        />
+      </div>
+      <div className={styles.openSourceItem}>
+        {/* @TODO fix links */}
+        <OpenSourceItem
+          docsUrl="/docs/asdlkfj"
+          githubUrl="/another/thing"
+          headingText="trufflepig"
+          textContent={MSG.descriptionTrufflepig}
+        />
+      </div>
+      <div className={styles.openSourceItem}>
+        {/* @TODO fix links */}
+        <OpenSourceItem
+          docsUrl="/docs/asdlkfj"
+          githubUrl="/another/thing"
+          headingText="purser"
+          textContent={MSG.descriptionPurser}
+        />
+      </div>
+      <div className={styles.openSourceItem}>
+        {/* @TODO fix links */}
+        <OpenSourceItem
+          docsUrl="/docs/asdlkfj"
+          githubUrl="/another/thing"
+          headingText="solidity-coverage"
+          textContent={MSG.descriptionSolidityCoverage}
+        />
+      </div>
     </div>
   </div>
 );
--- src/modules/pages/components/Developers/OpenSourceItem/OpenSourceItem.jsx
+++ src/modules/pages/components/Developers/OpenSourceItem/OpenSourceItem.jsx
@@ -1,9 +1,63 @@
 /* @flow */
+import type { MessageDescriptor } from 'react-intl';
+
 import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+import Heading from '~core/Heading';
+import Link from '~core/Link';
+
+import styles from './OpenSourceItem.module.css';
+
+const MSG = defineMessages({
+  docsLinkText: {
+    id: 'pages.Developers.OpenSourceItem.docsLinkText',
+    defaultMessage: 'Docs',
+  },
+});
+
+type Props = {|
+  docsUrl: string,
+  githubUrl: string,
+  headingText: string,
+  textContent: MessageDescriptor | string,
+|};
 
 const displayName = 'pages.Developers.OpenSourceItem';
 
-const OpenSourceItem = () => <div />;
+const OpenSourceItem = ({
+  docsUrl,
+  githubUrl,
+  headingText,
+  textContent,
+}: Props) => (
+  <>
+    <div className={styles.imageContainer}>{/* @TODO add image */}</div>
+    <div className={styles.contentContainer}>
+      <Heading
+        appearance={{ size: 'medium', theme: 'invert', weight: 'medium' }}
+        text={headingText}
+      />
+      <p>
+        {typeof textContent === 'string' ? (
+          textContent
+        ) : (
+          <FormattedMessage {...textContent} />
+        )}
+      </p>
+      <p>
+        <Link
+          className={styles.itemLink}
+          href={docsUrl}
+          text={MSG.docsLinkText}
+        />
+        <Link className={styles.itemLink} href={githubUrl}>
+          GitHub <span className={styles.linkArrow}>&rarr;</span>
+        </Link>
+      </p>
+    </div>
+  </>
+);
 
 OpenSourceItem.displayName = displayName;
 
