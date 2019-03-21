9e4822947aa31fd71810285d8b7f7d304594ce58
Curtis Olson <curtis@colony.io>
2019-03-20 19:40:02 -0500

    Add intro text & heading

--- src/modules/pages/components/Developers/OpenSource/OpenSource.jsx
+++ src/modules/pages/components/Developers/OpenSource/OpenSource.jsx
@@ -1,9 +1,37 @@
 /* @flow */
 import React from 'react';
+import { defineMessages, FormattedMessage } from 'react-intl';
+
+import Heading from '~core/Heading';
+
+import styles from './OpenSource.module.css';
+
+const MSG = defineMessages({
+  sectionIntroTitle: {
+    id: 'pages.Developers.OpenSource.sectionIntroTitle',
+    defaultMessage: 'Open Source Tools',
+  },
+  sectionIntroText: {
+    id: 'pages.Developers.OpenSource.sectionIntroText',
+    defaultMessage: 'Standalone tools for Ethereum developers.',
+  },
+});
 
 const displayName = 'pages.Developers.OpenSource';
 
-const OpenSource = () => <div />;
+const OpenSource = () => (
+  <div className={styles.main}>
+    <div className={styles.introSection}>
+      <Heading
+        appearance={{ margin: 'double', theme: 'invert', weight: 'medium' }}
+        text={MSG.sectionIntroTitle}
+      />
+      <p>
+        <FormattedMessage {...MSG.sectionIntroText} />
+      </p>
+    </div>
+  </div>
+);
 
 OpenSource.displayName = displayName;
 
