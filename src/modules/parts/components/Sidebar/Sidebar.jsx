b991554553863e8b331d5937df5e4f3b8ad4581b
Curtis Olson <curtis@colony.io>
2019-03-27 18:24:50 -0500

    Sidebar styling

--- src/modules/parts/components/Sidebar/Sidebar.jsx
+++ src/modules/parts/components/Sidebar/Sidebar.jsx
@@ -2,17 +2,23 @@
 import type { IntlShape } from 'react-intl';
 
 import React from 'react';
-import { defineMessages, injectIntl } from 'react-intl';
+import { defineMessages, FormattedMessage, injectIntl } from 'react-intl';
 
 import type { Project } from '~types';
 
 import Button from '~core/Button';
+import Heading from '~core/Heading';
 import Link from '~core/Link';
 import { getDocsForLocale, getSectionsForLocale, orderDocs } from '~utils/docs';
+import { PAGE_DEVELOPER_PORTAL } from '~routes';
 
 import styles from './Sidebar.module.css';
 
 const MSG = defineMessages({
+  linkHome: {
+    id: 'parts.Sidebar.linkHome',
+    defaultMessage: 'Home',
+  },
   btnBackToTop: {
     id: 'parts.Sidebar.btnBackToTop',
     defaultMessage: 'Back to Top',
@@ -26,12 +32,33 @@ type Props = {|
 
 const displayName = 'parts.Sidebar';
 
-const Sidebar = ({ intl: { locale }, project }: Props) => (
+const Sidebar = ({
+  intl: { locale },
+  project: { name: projectName },
+  project,
+}: Props) => (
   <nav className={styles.main}>
+    <Link className={styles.homeLink} href={PAGE_DEVELOPER_PORTAL}>
+      &larr; <FormattedMessage {...MSG.linkHome} />
+    </Link>
+    <div className={styles.projectTitle}>
+      <Heading
+        appearance={{ size: 'mediumLarge', theme: 'dark', weight: 'medium' }}
+        text={projectName}
+      />
+    </div>
     <ul className={styles.sectionsList}>
       {getSectionsForLocale(project, locale).map(({ docs, name, slug }) => (
         <li key={slug} className={styles.sectionItem}>
-          <h5 className={styles.sectionTitle}>{name}</h5>
+          <Heading
+            appearance={{
+              margin: 'double',
+              size: 'medium',
+              weight: 'medium',
+            }}
+            style={{ color: styles.textColor }}
+            text={name}
+          />
           <ul className={styles.docsList}>
             {getDocsForLocale(docs, locale)
               .sort(orderDocs)
