bfe47154c407a4db5df005781a0f767bff689a81
Curtis Olson <curtis@colony.io>
2019-03-31 14:26:49 -0500

    Add base `Tutorials` page template

---
+++ src/modules/pages/components/Tutorials/Tutorials.jsx
@@ -0,0 +1,10 @@
+/* @flow */
+import React from 'react';
+
+const displayName = 'pages.Tutorials';
+
+const Tutorials = () => <div />;
+
+Tutorials.displayName = displayName;
+
+export default Tutorials;
