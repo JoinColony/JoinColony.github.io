20f8fa3109adb2442d6e5973a6ce32862dfcd1d6
Curtis Olson <curtis@colony.io>
2019-03-31 14:27:44 -0500

    Add `tutorials` page

---
+++ src/pages/tutorials.js
@@ -0,0 +1,8 @@
+/* @flow */
+import { createElement } from 'react';
+
+import Tutorials from '~pages/Tutorials';
+
+const TutorialsPage = () => createElement(Tutorials);
+
+export default TutorialsPage;
