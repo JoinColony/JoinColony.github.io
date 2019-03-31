ab38d71a711b4e790bd6b2cc3063cec120065c93
Curtis Olson <curtis@colony.io>
2019-03-30 21:00:23 -0500

    Add blank `TutorialPage` component

---
+++ src/modules/pages/components/TutorialPage/TutorialPage.jsx
@@ -0,0 +1,10 @@
+/* @flow */
+import React from 'react';
+
+const displayName = 'pages.TutorialPage';
+
+const TutorialPage = () => <div />;
+
+TutorialPage.displayName = displayName;
+
+export default TutorialPage;
