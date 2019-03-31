c6f36c9bd9c5fbde02d075299d14badac9da6a90
Curtis Olson <curtis@colony.io>
2019-03-31 12:30:22 -0500

    Add page query using tutorialId from page context & fragment

--- src/modules/pages/components/TutorialPage/TutorialPage.jsx
+++ src/modules/pages/components/TutorialPage/TutorialPage.jsx
@@ -1,10 +1,17 @@
 /* @flow */
 import React from 'react';
+import { graphql } from 'gatsby';
 
 const displayName = 'pages.TutorialPage';
 
 const TutorialPage = () => <div />;
 
 TutorialPage.displayName = displayName;
+
+export const pageQuery = graphql`
+  query tutorialQuery($tutorialId: String!) {
+    ...singleTutorialFragment
+  }
+`;
 
 export default TutorialPage;
