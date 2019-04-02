ab9bdecbedee432cfde5dd095648f3bc9a076584
Curtis Olson <curtis@colony.io>
2019-04-02 14:53:29 -0500

    Improve SEO typing, refactor to functional component

---
+++ src/modules/parts/components/SEO/types.js
@@ -0,0 +1,25 @@
+/* @flow */
+import type { RouteProps } from '@reach/router';
+import type { IntlShape, MessageDescriptor } from 'react-intl';
+
+import type { FileContext as FileContextType } from '~types';
+
+export type InProps = {|
+  description: MessageDescriptor | string,
+  descriptionValues?: Object,
+  images?: Array<string>,
+  isDocPage?: boolean,
+  project?: string,
+  title: MessageDescriptor | string,
+  titleValues?: Object,
+|};
+
+export type OutProps = RouteProps &
+  InProps & {
+    baseUrl: string,
+    /** Injected by `injectIntl` */
+    intl: IntlShape,
+    files?: FileContextType,
+    getAbsoluteImagePath: (imagePath: string) => string,
+    siteLogo: string,
+  };
