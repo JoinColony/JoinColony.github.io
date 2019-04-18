5d15d65d1f05674cef0ac24021da7d76d66f3220
Curtis Olson <curtis@colony.io>
2019-04-18 18:42:54 -0500

    Make developer portal the homepage

--- src/pages/developers.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-import { createElement } from 'react';
-
-import Developers from '~pages/Developers';
-
-const DeveloperPortal = () => createElement(Developers);
-
-export default DeveloperPortal;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@
  */
 export const PAGE_INDEX = '/';
 export const PAGE_GET_STARTED_COLONYJS = '/docs/colonyjs/intro-get-started';
-export const PAGE_DEVELOPER_PORTAL = '/developers';
+export const PAGE_DEVELOPER_PORTAL = PAGE_INDEX;
 export const PAGE_GET_INVOLVED = '/get-involved';
 export const PAGE_LOGIN = '/login';
 export const PAGE_MEDIA_KIT = '/media-kit';
