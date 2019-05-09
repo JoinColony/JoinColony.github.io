cb141d971aade1d7f0f25aac00d69e8322be9a38
Ryan Christoffersen <ryan@ryanchristo.com>
2019-05-09 16:59:05 +0200

    Add email form handling

--- src/modules/pages/components/Dashboard/Account/Account.jsx
+++ src/modules/pages/components/Dashboard/Account/Account.jsx
@@ -3,7 +3,7 @@
 
 import type { WalletObjectType } from '@colony/purser-core';
 
-import React from 'react';
+import React, { useState } from 'react';
 // import Blockies from 'react-blockies';
 
 import { defineMessages, FormattedMessage } from 'react-intl';
@@ -38,13 +38,25 @@ const MSG = defineMessages({
     id: 'pages.Dashboard.Account.connectedAccountsEmailLabel',
     defaultMessage: 'Email',
   },
+  connectedAccountsCancel: {
+    id: 'pages.Dashboard.Account.connectedAccountsCancel',
+    defaultMessage: 'Cancel',
+  },
   connectedAccountsConnect: {
     id: 'pages.Dashboard.Account.connectedAccountsConnect',
     defaultMessage: 'Connect',
   },
+  connectedAccountsEdit: {
+    id: 'pages.Dashboard.Account.connectedAccountsEdit',
+    defaultMessage: 'Edit',
+  },
   connectedAccountsRemove: {
     id: 'pages.Dashboard.Account.connectedAccountsRemove',
     defaultMessage: 'Remove',
+  },
+  connectedAccountsSave: {
+    id: 'pages.Dashboard.Account.connectedAccountsSave',
+    defaultMessage: 'Save',
   },
 });
 
@@ -65,79 +77,60 @@ const Account = ({
   discourse,
   github,
   wallet,
-}: Props) => (
-  <>
-    <div className={styles.main}>
-      <div className={styles.header}>
-        <Image
-          className={styles.photo}
-          alt={github.name || github.username}
-          src={github.photo}
-        />
-        <div>
-          <div className={styles.name}>{github.name || github.username}</div>
-          <div className={styles.address}>
-            {/*
-            <Blockies
-              className={styles.blockies}
-              seed={wallet.address}
-              scale={2.5}
-            />
-            */}
-            {wallet.address}
-          </div>
-          <div className={styles.statistics}>
-            <div className={styles.statistic}>
-              <span className={styles.statisticValue}>0</span>
-              <span className={styles.statisticLabel}>CLNY</span>
+}: Props) => {
+  const [editEmail, setEditEmail] = useState(false);
+  const [email, setEmail] = useState(
+    github.email || (discourse && discourse.email),
+  );
+  const handleChangeEmail = event => {
+    setEmail(event.target.value);
+  };
+  const handleSaveEmail = () => {
+    setEditEmail(false);
+  };
+  return (
+    <>
+      <div className={styles.main}>
+        <div className={styles.header}>
+          <Image
+            className={styles.photo}
+            alt={github.name || github.username}
+            src={github.photo}
+          />
+          <div>
+            <div className={styles.name}>{github.name || github.username}</div>
+            <div className={styles.address}>
+              {/*
+              <Blockies
+                className={styles.blockies}
+                seed={wallet.address}
+                scale={2.5}
+              />
+              */}
+              {wallet.address}
             </div>
-            <div className={styles.statistic}>
-              <span className={styles.statisticValue}>0</span>
-              <span className={styles.statisticLabel}>Reputation</span>
+            <div className={styles.statistics}>
+              <div className={styles.statistic}>
+                <span className={styles.statisticValue}>0</span>
+                <span className={styles.statisticLabel}>CLNY</span>
+              </div>
+              <div className={styles.statistic}>
+                <span className={styles.statisticValue}>0</span>
+                <span className={styles.statisticLabel}>Reputation</span>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.contentColumn}>
-          <h2 className={styles.contentTitle}>
-            <FormattedMessage {...MSG.connectedAccountsTitle} />
-          </h2>
-          <p>
-            <FormattedMessage {...MSG.connectedAccountsDescription} />
-          </p>
-        </div>
-        <div className={styles.contentColumn}>
-          <div className={styles.field}>
-            <Input
-              disabled
-              appearance={{
-                padding: 'huge',
-                width: 'large',
-              }}
-              id="github"
-              label={MSG.connectedAccountsGitHubLabel}
-              type="text"
-              value={`@${github.username}`}
-            />
-            {github ? (
-              <Button
-                appearance={{ theme: 'reset', color: 'blue', weight: 'bold' }}
-                onClick={() => disconnect('github')}
-                text={MSG.connectedAccountsRemove}
-                type="submit"
-              />
-            ) : (
-              <Button
-                appearance={{ theme: 'primary', padding: 'large' }}
-                onClick={() => authenticate('github')}
-                style={{ margin: '12px auto' }}
-                text={MSG.connectedAccountsConnect}
-                type="submit"
-              />
-            )}
+        <div className={styles.content}>
+          <div className={styles.contentColumn}>
+            <h2 className={styles.contentTitle}>
+              <FormattedMessage {...MSG.connectedAccountsTitle} />
+            </h2>
+            <p>
+              <FormattedMessage {...MSG.connectedAccountsDescription} />
+            </p>
           </div>
-          {(github.email || (discourse && discourse.email)) && (
+          <div className={styles.contentColumn}>
             <div className={styles.field}>
               <Input
                 disabled
@@ -145,48 +138,103 @@ const Account = ({
                   padding: 'huge',
                   width: 'large',
                 }}
-                id="email"
-                label={MSG.connectedAccountsEmailLabel}
+                id="github"
+                label={MSG.connectedAccountsGitHubLabel}
                 type="text"
-                value={github.email || (discourse && discourse.email)}
+                value={`@${github.username}`}
               />
+              {github ? (
+                <Button
+                  appearance={{ theme: 'reset', color: 'blue', weight: 'bold' }}
+                  onClick={() => disconnect('github')}
+                  text={MSG.connectedAccountsRemove}
+                  type="submit"
+                />
+              ) : (
+                <Button
+                  appearance={{ theme: 'primary', padding: 'large' }}
+                  onClick={() => authenticate('github')}
+                  style={{ margin: '12px auto' }}
+                  text={MSG.connectedAccountsConnect}
+                  type="submit"
+                />
+              )}
             </div>
-          )}
-          <div className={styles.field}>
-            <Input
-              disabled
-              appearance={{
-                display: discourse ? undefined : 'none',
-                padding: 'huge',
-                width: 'large',
-              }}
-              id="discourse"
-              label={MSG.connectedAccountsDiscourseLabel}
-              type="text"
-              value={discourse ? `@${discourse.username}` : ''}
-            />
-            {discourse ? (
-              <Button
-                appearance={{ theme: 'reset', color: 'blue', weight: 'bold' }}
-                onClick={() => disconnect('discourse')}
-                text={MSG.connectedAccountsRemove}
-                type="submit"
-              />
-            ) : (
-              <Button
-                appearance={{ theme: 'primary', padding: 'large' }}
-                onClick={() => authenticate('discourse')}
-                style={{ margin: '12px auto' }}
-                text={MSG.connectedAccountsConnect}
-                type="submit"
-              />
+            {(github.email || (discourse && discourse.email)) && (
+              <div className={styles.field}>
+                <Input
+                  disabled={!editEmail}
+                  appearance={{
+                    padding: 'huge',
+                    width: 'large',
+                  }}
+                  id="email"
+                  label={MSG.connectedAccountsEmailLabel}
+                  onChange={handleChangeEmail}
+                  type="text"
+                  value={email}
+                />
+                {editEmail && (
+                  <Button
+                    appearance={{
+                      theme: 'reset',
+                      color: 'blue',
+                      weight: 'bold',
+                    }}
+                    onClick={handleSaveEmail}
+                    style={{ marginRight: '15px' }}
+                    text={MSG.connectedAccountsSave}
+                    type="submit"
+                  />
+                )}
+                <Button
+                  appearance={{ theme: 'reset', color: 'blue', weight: 'bold' }}
+                  onClick={() => setEditEmail(!editEmail)}
+                  text={
+                    editEmail
+                      ? MSG.connectedAccountsCancel
+                      : MSG.connectedAccountsEdit
+                  }
+                  type="submit"
+                />
+              </div>
             )}
+            <div className={styles.field}>
+              <Input
+                disabled
+                appearance={{
+                  display: discourse ? undefined : 'none',
+                  padding: 'huge',
+                  width: 'large',
+                }}
+                id="discourse"
+                label={MSG.connectedAccountsDiscourseLabel}
+                type="text"
+                value={discourse ? `@${discourse.username}` : ''}
+              />
+              {discourse ? (
+                <Button
+                  appearance={{ theme: 'reset', color: 'blue', weight: 'bold' }}
+                  onClick={() => disconnect('discourse')}
+                  text={MSG.connectedAccountsRemove}
+                  type="submit"
+                />
+              ) : (
+                <Button
+                  appearance={{ theme: 'primary', padding: 'large' }}
+                  onClick={() => authenticate('discourse')}
+                  style={{ margin: '12px auto' }}
+                  text={MSG.connectedAccountsConnect}
+                  type="submit"
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 Account.displayName = displayName;
 
