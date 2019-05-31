c93071cd0c95f771673262e73563a3bfcd6ecdc4
Ryan Christoffersen <ryan@ryanchristo.com>
2019-05-31 16:57:56 +0200

    Update task actions

--- src/modules/pages/components/Contribute/Task/Task.jsx
+++ src/modules/pages/components/Contribute/Task/Task.jsx
@@ -3,8 +3,9 @@
 import type { ColonyNetworkClient } from '@colony/colony-js-client';
 import type { WalletObjectType } from '@colony/purser-core';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { defineMessages, FormattedMessage } from 'react-intl';
+import { sha3 } from 'web3-utils';
 
 import Button from '~core/Button';
 
@@ -28,6 +29,26 @@ const MSG = defineMessages({
     id: 'pages.Contribute.Task.buttonStartWork',
     defaultMessage: 'Start Work',
   },
+  buttonSubmitWork: {
+    id: 'pages.Contribute.Task.buttonSubmitWork',
+    defaultMessage: 'Submit Work',
+  },
+  buttonSubmitRating: {
+    id: 'pages.Contribute.Task.buttonSubmitRating',
+    defaultMessage: 'Submit Rating',
+  },
+  buttonRevealRating: {
+    id: 'pages.Contribute.Task.buttonRevealRating',
+    defaultMessage: 'Reveal Rating',
+  },
+  buttonFinalizeTask: {
+    id: 'pages.Contribute.Task.buttonFinalizeTask',
+    defaultMessage: 'Finalize Task',
+  },
+  buttonClaimPayout: {
+    id: 'pages.Contribute.Task.buttonClaimPayout',
+    defaultMessage: 'Claim Payout',
+  },
 });
 
 const displayName = 'pages.Contribute.Task';
@@ -43,33 +64,49 @@ const Task = ({ networkClient, wallet }: Props) => {
   const [task, setTask] = useState(null);
   const [pendingOperation, setPendingOperation] = useState(null);
 
+  const taskId: ?number = useMemo(() => {
+    if (typeof window !== 'undefined') {
+      const taskIdString = window.location.search.split('id=')[1];
+      if (taskIdString) return Number(taskIdString);
+      return null;
+    }
+    return null;
+  }, []);
+
   useEffect(() => {
-    if (!loadedLocal) {
-      const localTask = getStore('task_1');
-      const localOperation = getStore('task_1_operation');
+    if (!loadedLocal && taskId) {
+      const localTask = getStore(`task_${taskId}`);
+      const localOperation = getStore(`task_${taskId}_operation`);
       setTask(localTask);
       setPendingOperation(localOperation);
       setLoadedLocal(true);
     }
-  }, [task, loadedLocal]);
-
-  useEffect(() => setStore('task_1', task), [task]);
-  useEffect(() => setStore('task_1_operation', pendingOperation), [
-    pendingOperation,
-  ]);
+  }, [task, loadedLocal, taskId]);
 
   useEffect(() => {
-    if (!loadedTask && networkClient) {
+    if (taskId) setStore(`task_${taskId}`, task);
+  }, [task, taskId]);
+
+  useEffect(() => {
+    if (taskId) setStore(`task_${taskId}_operation`, pendingOperation);
+  }, [pendingOperation, taskId]);
+
+  useEffect(() => {
+    if (!loadedTask && networkClient && taskId) {
       (async () => {
         setLoadedTask(true);
         const colonyClient = await networkClient.getColonyClientByAddress(
           '0x0a97cb5A59085C0d5903622b3635D107Ab8F20AE',
         );
         const result = await colonyClient.getTask.call({
-          taskId: 1,
+          taskId,
+        });
+        const manager = await colonyClient.getTaskRole.call({
+          taskId,
+          role: 'MANAGER',
         });
         const worker = await colonyClient.getTaskRole.call({
-          taskId: result.id,
+          taskId,
           role: 'WORKER',
         });
         const pot = await colonyClient.getFundingPot.call({
@@ -81,6 +118,7 @@ const Task = ({ networkClient, wallet }: Props) => {
         });
         setTask({
           ...result,
+          manager,
           worker,
           pot: { payout: payout.toString(), ...pot },
         });
@@ -88,38 +126,100 @@ const Task = ({ networkClient, wallet }: Props) => {
     }
   });
 
-  const handleStartWork = async () => {
-    const colonyClient = await networkClient.getColonyClientByAddress(
-      '0x0a97cb5A59085C0d5903622b3635D107Ab8F20AE',
-    );
-    const operation = await colonyClient.setTaskWorkerRole.startOperation({
-      taskId: 1,
-      user: wallet.address,
-    });
-    await operation.sign();
+  const handleApproveWorker = async () => {
+    if (taskId) {
+      // TODO Get the operation from the database
+      const operationJSON = getStore(`task_${taskId}_operation`);
 
-    // TODO Save the operation to the database
-    setStore('task_1_operation', operation.toJSON());
+      const colonyClient = await networkClient.getColonyClientByAddress(
+        '0x0a97cb5A59085C0d5903622b3635D107Ab8F20AE',
+      );
+      const operation = await colonyClient.setTaskWorkerRole.restoreOperation(
+        operationJSON,
+      );
+      setPendingOperation(operation);
+      await operation.sign();
+      // TODO The gas limit needs to be fixed in colonyJS
+      await operation.send({ gasLimit: 1000000 });
+      setPendingOperation(null);
+
+      // TODO Remove the opetation from the database
+      setStore(`task_${taskId}_operation`, null);
+    }
   };
 
-  const handleApproveWorker = async () => {
-    // TODO Get the operation from the database
-    const operationJSON = getStore('task_1_operation');
+  const handleStartWork = async () => {
+    if (taskId) {
+      const colonyClient = await networkClient.getColonyClientByAddress(
+        '0x0a97cb5A59085C0d5903622b3635D107Ab8F20AE',
+      );
+      const operation = await colonyClient.setTaskWorkerRole.startOperation({
+        taskId,
+        user: wallet.address,
+      });
+      await operation.sign();
 
+      // TODO Save the operation to the database
+      setStore(`task_${taskId}_operation`, operation.toJSON());
+    }
+  };
+
+  const handleSubmitWork = async () => {
+    if (task) {
+      const colonyClient = await networkClient.getColonyClientByAddress(
+        '0x0a97cb5A59085C0d5903622b3635D107Ab8F20AE',
+      );
+      await colonyClient.submitTaskDeliverable.send({
+        taskId,
+        deliverableHash: task.specificationHash,
+      });
+    }
+  };
+
+  const handleSubmitRating = async () => {
     const colonyClient = await networkClient.getColonyClientByAddress(
       '0x0a97cb5A59085C0d5903622b3635D107Ab8F20AE',
     );
-    const operation = await colonyClient.setTaskWorkerRole.restoreOperation(
-      operationJSON,
-    );
-    setPendingOperation(operation);
-    await operation.sign();
-    // TODO The gas limit needs to be fixed in colonyJS
-    await operation.send({ gasLimit: 1000000 });
-    setPendingOperation(null);
+    const { secret } = await colonyClient.generateSecret.call({
+      salt: sha3('secret'),
+      value: 2,
+    });
+    await colonyClient.submitTaskWorkRating.send({
+      taskId,
+      role: 'MANAGER',
+      secret,
+    });
+  };
 
-    // TODO Remove the opetation from the database
-    setStore('task_1_operation', null);
+  const handleRevealRating = async () => {
+    const colonyClient = await networkClient.getColonyClientByAddress(
+      '0x0a97cb5A59085C0d5903622b3635D107Ab8F20AE',
+    );
+    const salt = sha3('secret');
+    await colonyClient.revealTaskWorkRating.send({
+      taskId,
+      role: 'MANAGER',
+      rating: 2,
+      salt,
+    });
+  };
+
+  const handleFinalizeTask = async () => {
+    const colonyClient = await networkClient.getColonyClientByAddress(
+      '0x0a97cb5A59085C0d5903622b3635D107Ab8F20AE',
+    );
+    await colonyClient.finalizeTask.send({ taskId });
+  };
+
+  const handleClaimPayout = async () => {
+    const colonyClient = await networkClient.getColonyClientByAddress(
+      '0x0a97cb5A59085C0d5903622b3635D107Ab8F20AE',
+    );
+    await colonyClient.claimTaskPayout.send({
+      taskId,
+      role: 'WORKER',
+      token: colonyClient.tokenClient.contract.address,
+    });
   };
 
   return (
@@ -154,22 +254,98 @@ const Task = ({ networkClient, wallet }: Props) => {
         ) : (
           <p>loading...</p>
         )}
-        <div className={styles.buttons}>
-          <Button
-            appearance={{ theme: 'primary' }}
-            disabled={!task || (task && task.worker)}
-            onClick={handleStartWork}
-            text={MSG.buttonStartWork}
-            type="submit"
-          />
-          <Button
-            appearance={{ theme: 'primary' }}
-            disabled={!pendingOperation}
-            onClick={handleApproveWorker}
-            text={MSG.buttonApproveWorker}
-            type="submit"
-          />
-        </div>
+        {task && !task.worker && (
+          <div className={styles.buttons}>
+            <Button
+              appearance={{ theme: 'primary' }}
+              disabled={!task || (task && task.worker)}
+              onClick={handleStartWork}
+              text={MSG.buttonStartWork}
+              type="submit"
+            />
+          </div>
+        )}
+        {task &&
+          task.worker &&
+          task.worker.address &&
+          task.worker.address.toLowerCase() === wallet.address && (
+            <div className={styles.buttons}>
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleSubmitWork}
+                text={MSG.buttonSubmitWork}
+                type="submit"
+              />
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleSubmitRating}
+                text={MSG.buttonSubmitRating}
+                type="submit"
+              />
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleRevealRating}
+                text={MSG.buttonRevealRating}
+                type="submit"
+              />
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleRevealRating}
+                text={MSG.buttonRevealRating}
+                type="submit"
+              />
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleClaimPayout}
+                text={MSG.buttonClaimPayout}
+                type="submit"
+              />
+            </div>
+          )}
+        {task &&
+          task.manager &&
+          task.manager.address &&
+          task.manager.address.toLowerCase() === wallet.address && (
+            <div className={styles.buttons}>
+              <Button
+                appearance={{ theme: 'primary' }}
+                disabled={!pendingOperation}
+                onClick={handleApproveWorker}
+                text={MSG.buttonApproveWorker}
+                type="submit"
+              />
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleSubmitRating}
+                text={MSG.buttonSubmitRating}
+                type="submit"
+              />
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleRevealRating}
+                text={MSG.buttonRevealRating}
+                type="submit"
+              />
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleRevealRating}
+                text={MSG.buttonRevealRating}
+                type="submit"
+              />
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleFinalizeTask}
+                text={MSG.buttonFinalizeTask}
+                type="submit"
+              />
+              <Button
+                appearance={{ theme: 'primary' }}
+                onClick={handleClaimPayout}
+                text={MSG.buttonClaimPayout}
+                type="submit"
+              />
+            </div>
+          )}
       </div>
     </>
   );
