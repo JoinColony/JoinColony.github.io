ba2c4be932e0c885e49e65ccab833664e12b5edd
Ryan Christoffersen <ryan@ryanchristo.com>
2019-06-11 21:38:52 +0700

    Updated formatted token

--- src/modules/core/components/FormattedToken/FormattedToken.jsx
+++ src/modules/core/components/FormattedToken/FormattedToken.jsx
@@ -33,13 +33,17 @@ type Props = {
   /** Appearance object */
   appearance?: Appearance,
   /** Token amount */
-  amount: number | string,
+  amount: number,
   /** Overwriting class name(s). Setting this will overwrite the `appearance` object */
   className?: string,
   /** Token amount */
   decimals?: number,
   /** Injected by `injectIntl` */
   intl: IntlShape,
+  /** Maximum number of fraction digits */
+  maximumFractionDigits?: number,
+  /** Minimum number of fraction digits */
+  minimumFractionDigits?: number,
   /** Token symbol */
   symbol: string,
 };
@@ -51,19 +55,24 @@ const FormattedToken = ({
   appearance,
   className,
   decimals,
+  maximumFractionDigits,
+  minimumFractionDigits,
   symbol,
 }: Props) => {
   const classNames = className || getMainClasses(appearance, styles);
+  const formattedAmount = amount / 10 ** (decimals || 18);
   const tokenFormat = {
-    id: 'CDEV',
-    maximumFractionDigits: decimals || 18,
-    minimumFractionDigits: 0,
+    id: symbol,
+    maximumFractionDigits: maximumFractionDigits || 4,
+    minimumFractionDigits: minimumFractionDigits || 0,
   };
   return (
     <div className={classNames}>
       <FormattedMessage
         {...MSG.amount}
-        values={{ amount: <FormattedNumber {...tokenFormat} value={amount} /> }}
+        values={{
+          amount: <FormattedNumber {...tokenFormat} value={formattedAmount} />,
+        }}
       />
       <FormattedMessage {...MSG.symbol} values={{ symbol }} />
     </div>
