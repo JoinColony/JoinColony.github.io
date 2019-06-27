ffbd8c10a44bf2a6560b85c0e098afb0b9fe361c
Curtis Olson <curtis@colony.io>
2019-06-27 18:24:16 -0500

    Add `Header` and `Footer`

--- src/modules/layouts/components/WebsiteLayout/WebsiteLayout.jsx
+++ src/modules/layouts/components/WebsiteLayout/WebsiteLayout.jsx
@@ -4,13 +4,22 @@ import type { Element } from 'react';
 
 import React, { Component } from 'react';
 
+import Footer from './Footer';
+import Header from './Header';
+
 type Props = {|
   children: Element<typeof Component>,
 |};
 
 const displayName = 'layouts.WebsiteLayout';
 
-const WebsiteLayout = ({ children }: Props) => <>{children}</>;
+const WebsiteLayout = ({ children }: Props) => (
+  <>
+    <Header />
+    {children}
+    <Footer />
+  </>
+);
 
 WebsiteLayout.displayName = displayName;
 
