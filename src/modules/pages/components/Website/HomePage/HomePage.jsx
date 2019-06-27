5544ddbb26941dd76fa71ce2be8bd17b8be5e129
Curtis Olson <curtis@colony.io>
2019-06-27 18:30:26 -0500

    Add homepage to temporary location

---
+++ src/modules/pages/components/Website/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+/* @flow */
+
+import React from 'react';
+
+import WebsiteLayout from '~layouts/WebsiteLayout';
+
+const displayName = 'pages.Website.HomePage';
+
+const HomePage = () => (
+  <WebsiteLayout>
+    <div />
+  </WebsiteLayout>
+);
+
+HomePage.displayName = displayName;
+
+export default HomePage;
---
+++ src/pages/website/index.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+import { createElement } from 'react';
+
+import HomePage from '~pages/Website/HomePage';
+
+const WebsiteHomePage = () => createElement(HomePage);
+
+export default WebsiteHomePage;
