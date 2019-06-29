5809da237287a7129e21e32c778bcd29cca73d9d
Curtis Olson <curtis@colony.io>
2019-06-29 12:52:16 -0500

    Add `TLDR` section

--- src/modules/pages/components/Website/HomePage/HomePage.jsx
+++ src/modules/pages/components/Website/HomePage/HomePage.jsx
@@ -5,12 +5,14 @@ import React from 'react';
 import WebsiteLayout from '~layouts/WebsiteLayout';
 
 import Hero from './Hero';
+import TLDR from './TLDR';
 
 const displayName = 'pages.Website.HomePage';
 
 const HomePage = () => (
   <WebsiteLayout transparentNav>
     <Hero />
+    <TLDR />
   </WebsiteLayout>
 );
 
