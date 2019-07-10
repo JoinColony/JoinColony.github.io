5dc9eb6bbfbd3459f6f04956f69dd2d22d0c4a55
Curtis Olson <curtis@colony.io>
2019-07-10 16:55:35 -0500

    Add about page

---
+++ src/modules/pages/components/Website/About/About.jsx
@@ -0,0 +1,17 @@
+/* @flow */
+
+import React from 'react';
+
+import WebsiteLayout from '~layouts/WebsiteLayout';
+
+const displayName = 'pages.Website.About';
+
+const About = () => (
+  <WebsiteLayout transparentNav>
+    <div />
+  </WebsiteLayout>
+);
+
+About.displayName = displayName;
+
+export default About;
