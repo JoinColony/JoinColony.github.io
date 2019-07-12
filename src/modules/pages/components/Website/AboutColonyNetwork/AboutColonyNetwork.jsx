c67d4fa5d4951c95f56a930b7dd48b22e4e74129
Curtis Olson <curtis@colony.io>
2019-07-12 15:50:37 -0500

    Add "About Colony Network" page

---
+++ src/modules/pages/components/Website/AboutColonyNetwork/AboutColonyNetwork.jsx
@@ -0,0 +1,17 @@
+/* @flow */
+
+import React from 'react';
+
+import WebsiteLayout from '~layouts/WebsiteLayout';
+
+const displayName = 'pages.Website.AboutColonyNetwork';
+
+const AboutColonyNetwork = () => (
+  <WebsiteLayout headerAppearance={{ theme: 'transparent', navTheme: 'dark' }}>
+    <div />
+  </WebsiteLayout>
+);
+
+AboutColonyNetwork.displayName = displayName;
+
+export default AboutColonyNetwork;
