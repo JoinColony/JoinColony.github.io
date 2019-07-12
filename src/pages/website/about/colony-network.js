87d6d89d0b23d68352464d84d4bbc5fd83c9b790
Curtis Olson <curtis@colony.io>
2019-07-12 15:50:37 -0500

    Add "About Colony Network" page

---
+++ src/pages/website/about/colony-network.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+import { createElement } from 'react';
+
+import AboutColonyNetwork from '~pages/Website/AboutColonyNetwork';
+
+const AboutColonyNetworkPage = () => createElement(AboutColonyNetwork);
+
+export default AboutColonyNetworkPage;
