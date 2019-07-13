0b6b0285459734cce81f6bb40d4828a5d14b2f06
Curtis Olson <curtis@colony.io>
2019-07-12 19:09:21 -0500

    Add `Hero` section to about colony network page

--- src/modules/pages/components/Website/AboutColonyNetwork/AboutColonyNetwork.jsx
+++ src/modules/pages/components/Website/AboutColonyNetwork/AboutColonyNetwork.jsx
@@ -1,16 +1,30 @@
 /* @flow */
 
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import WebsiteLayout from '~layouts/WebsiteLayout';
 
+import Hero from './Hero';
+
 const displayName = 'pages.Website.AboutColonyNetwork';
 
-const AboutColonyNetwork = () => (
-  <WebsiteLayout headerAppearance={{ theme: 'transparent', navTheme: 'dark' }}>
-    <div />
-  </WebsiteLayout>
-);
+const AboutColonyNetwork = () => {
+  const { project } = useStaticQuery(graphql`
+    query aboutColonyNetworkProjectQuery(
+      $projectName: String = "colonyNetwork"
+    ) {
+      ...singleProjectFragment
+    }
+  `);
+  return (
+    <WebsiteLayout
+      headerAppearance={{ theme: 'transparent', navTheme: 'dark' }}
+    >
+      <Hero project={project} />
+    </WebsiteLayout>
+  );
+};
 
 AboutColonyNetwork.displayName = displayName;
 
