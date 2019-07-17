9fec01de5393ece635db28ef8d5753e66c5fc371
Curtis Olson <curtis@colony.io>
2019-07-17 18:53:19 -0500

    Add `AboutMetaColony` page

---
+++ src/modules/pages/components/Website/AboutMetaColony/AboutMetaColony.jsx
@@ -0,0 +1,17 @@
+/* @flow */
+
+import React from 'react';
+
+import WebsiteLayout from '~layouts/WebsiteLayout';
+
+const displayName = 'pages.Website.AboutMetaColony';
+
+const AboutMetaColony = () => (
+  <WebsiteLayout headerAppearance={{ navTheme: 'dark', theme: 'transparent' }}>
+    <div />
+  </WebsiteLayout>
+);
+
+AboutMetaColony.displayName = displayName;
+
+export default AboutMetaColony;
