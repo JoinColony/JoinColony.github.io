797d64f68d51ebe96f75ef7622fad087f7977a53
Curtis Olson <curtis@colony.io>
2019-07-29 16:14:31 -0400

    Add `ProductPlatform` page

---
+++ src/modules/pages/components/Website/ProductPlatform/ProductPlatform.jsx
@@ -0,0 +1,17 @@
+/* @flow */
+
+import React from 'react';
+
+import WebsiteLayout from '~layouts/WebsiteLayout';
+
+const displayName = 'pages.Website.ProductPlatform';
+
+const ProductPlatform = () => (
+  <WebsiteLayout>
+    <div />
+  </WebsiteLayout>
+);
+
+ProductPlatform.displayName = displayName;
+
+export default ProductPlatform;
---
+++ src/pages/product/index.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+import { createElement } from 'react';
+
+import ProductPlatform from '~pages/Website/ProductPlatform';
+
+const ProductPlatformPage = () => createElement(ProductPlatform);
+
+export default ProductPlatformPage;
