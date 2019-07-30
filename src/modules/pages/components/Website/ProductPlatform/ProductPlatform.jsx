286b0602668c6cbf5a499386d5359296d5ee3b41
Curtis Olson <curtis@colony.io>
2019-07-30 16:52:21 -0400

    Add `LookGood` section to product platform page

--- src/modules/pages/components/Website/ProductPlatform/ProductPlatform.jsx
+++ src/modules/pages/components/Website/ProductPlatform/ProductPlatform.jsx
@@ -5,12 +5,14 @@ import React from 'react';
 import WebsiteLayout from '~layouts/WebsiteLayout';
 
 import Hero from './Hero';
+import LookGood from './LookGood';
 
 const displayName = 'pages.Website.ProductPlatform';
 
 const ProductPlatform = () => (
   <WebsiteLayout headerAppearance={{ theme: 'transparent', logoTheme: 'dark' }}>
     <Hero />
+    <LookGood />
   </WebsiteLayout>
 );
 
