64aa1ed6750dcb5ddb3606320c262f683eb3ccda
Curtis Olson <curtis@colony.io>
2019-07-31 15:06:00 -0400

    Add `Examples` section to product platform page

--- src/modules/pages/components/Website/ProductPlatform/ProductPlatform.jsx
+++ src/modules/pages/components/Website/ProductPlatform/ProductPlatform.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 
 import WebsiteLayout from '~layouts/WebsiteLayout';
 
+import Examples from './Examples';
 import Hero from './Hero';
 import LookGood from './LookGood';
 
@@ -13,6 +14,7 @@ const ProductPlatform = () => (
   <WebsiteLayout headerAppearance={{ theme: 'transparent', logoTheme: 'dark' }}>
     <Hero />
     <LookGood />
+    <Examples />
   </WebsiteLayout>
 );
 
