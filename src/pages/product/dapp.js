8f495fffabb53914ccdf1dbba9b04826ed89cab7
Curtis Olson <curtis@colony.io>
2019-08-06 17:11:02 -0500

    Add `ProductDapp` page

---
+++ src/pages/product/dapp.js
@@ -0,0 +1,9 @@
+/* @flow */
+
+import { createElement } from 'react';
+
+import ProductDapp from '~pages/Website/ProductDapp';
+
+const ProductDappPage = () => createElement(ProductDapp);
+
+export default ProductDappPage;
