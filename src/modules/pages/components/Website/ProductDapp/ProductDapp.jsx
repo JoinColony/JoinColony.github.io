b6f72e2fe709e3a32ff27eff748bd43705ab332b
Curtis Olson <curtis@colony.io>
2019-08-06 17:11:02 -0500

    Add `ProductDapp` page

---
+++ src/modules/pages/components/Website/ProductDapp/ProductDapp.jsx
@@ -0,0 +1,17 @@
+/* @flow */
+
+import React from 'react';
+
+import WebsiteLayout from '~layouts/WebsiteLayout';
+
+const displayName = 'pages.Website.ProductDapp';
+
+const ProductDapp = () => (
+  <WebsiteLayout>
+    <div />
+  </WebsiteLayout>
+);
+
+ProductDapp.displayName = displayName;
+
+export default ProductDapp;
