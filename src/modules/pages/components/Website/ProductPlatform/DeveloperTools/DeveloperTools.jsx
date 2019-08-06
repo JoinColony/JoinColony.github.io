b7653b19f692e7a642f7543427845b93110ac42e
Curtis Olson <curtis@colony.io>
2019-08-06 13:54:39 -0500

    Add other projects to tools section, fix dynamic content

--- src/modules/pages/components/Website/ProductPlatform/DeveloperTools/DeveloperTools.jsx
+++ src/modules/pages/components/Website/ProductPlatform/DeveloperTools/DeveloperTools.jsx
@@ -12,43 +12,27 @@ import Paragraph from '~core/Paragraph';
 import {
   DOCS_COLONY_JS_GET_STARTED,
   DOCS_COLONY_NETWORK,
+  DOCS_COLONY_PURSER,
   DOCS_COLONY_STARTER,
+  DOCS_COLONY_TAILOR,
   COLONY_GITHUB_COLONYJS,
   COLONY_GITHUB_COLONYNETWORK,
   COLONY_GITHUB_COLONYSTARTER,
+  COLONY_GITHUB_PINION,
+  COLONY_GITHUB_PURSER,
+  COLONY_GITHUB_TAILOR,
+  COLONY_GITHUB_TRUFFLEPIG,
+  GITHUB_SOLIDITY_COVERAGE,
 } from '~routes';
 
 import styles from './DeveloperTools.module.css';
 
 const MSG = defineMessages({
-  bodyGridItem0: {
-    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyGridItem0',
-    defaultMessage: `A JavaScript library with a simple and predictable
-      interface for application-layer integrations with the colonyNetwork
-      smart contracts.`,
-  },
-  bodyGridItem1: {
-    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyGridItem1',
-    defaultMessage: `Boilerplates and examples to get started. Spin up an
-      application in minutes or create an extension for the Colony Network
-      smart contracts.`,
-  },
-  bodyGridItem2: {
-    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyGridItem2',
-    // @TODO update copy with colonyNetwork text
-    defaultMessage: `Pre-build UI components making it easy to use Colony
-      in your environment, or develop UI modules for the Colony app.`,
-  },
-  bodyTopic0: {
-    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyTopic0',
+  body: {
+    id: 'pages.Website.ProductPlatform.DeveloperTools.body',
     defaultMessage: `Where we have a need not met by existing tooling,
       we build generic open source solutions. It’s the least we can do
       to support a movement from which we all benefit daily.`,
-  },
-  bodyTopic1: {
-    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyTopic0',
-    // @TODO update this content
-    defaultMessage: 'Content not finished',
   },
   buttonTopic0: {
     id: 'pages.Website.ProductPlatform.DeveloperTools.buttonTopic0',
@@ -72,6 +56,55 @@ const MSG = defineMessages({
   },
 });
 
+const MSG_COMMUNITY = defineMessages({
+  bodyPinion: {
+    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyPinion',
+    defaultMessage: 'Pin orbit-db stores and ipfs hashes',
+  },
+  bodyPurser: {
+    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyPurser',
+    defaultMessage: `A JavaScript library that simplifies interaction
+      with Ethereum wallets`,
+  },
+  bodyTailor: {
+    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyTailor',
+    defaultMessage: `A JavaScript library that provides an easy-to-use
+      layer between lower-level libraries and your dapp`,
+  },
+  bodyTrufflePig: {
+    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyTrufflePig',
+    defaultMessage: `Truffle contract artifact loading tool for local
+      development`,
+  },
+  bodySolidityCoverage: {
+    id: 'pages.Website.ProductPlatform.DeveloperTools.bodySolidityCoverage',
+    defaultMessage: `Boilerplates and examples to get started. Spin up an
+      application in minutes or create an extension for the Colony Network
+      smart contracts.`,
+  },
+});
+
+const MSG_CORE = defineMessages({
+  bodyColonyJS: {
+    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyColonyJS',
+    defaultMessage: `A JavaScript library with a simple and predictable
+      interface for application-layer integrations with the colonyNetwork
+      smart contracts.`,
+  },
+  bodyColonyStarter: {
+    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyColonyStarter',
+    defaultMessage: `Boilerplates and examples to get started. Spin up an
+      application in minutes or create an extension for the Colony Network
+      smart contracts.`,
+  },
+  bodyColonyNetwork: {
+    id: 'pages.Website.ProductPlatform.DeveloperTools.bodyColonyNetwork',
+    // @TODO update copy with colonyNetwork text
+    defaultMessage: `Pre-build UI components making it easy to use Colony
+      in your environment, or develop UI modules for the Colony app.`,
+  },
+});
+
 const GridItem = ({
   body,
   iconName,
@@ -81,7 +114,7 @@ const GridItem = ({
 }: {
   body: MessageDescriptor,
   iconName: string,
-  linkDocs: string,
+  linkDocs?: string,
   linkGithub: string,
   title: string,
 }) => (
@@ -102,7 +135,7 @@ const GridItem = ({
       <Paragraph text={body} />
     </div>
     <div className={styles.itemLinks}>
-      <Link href={linkDocs} text={MSG.linkTextDocs} />
+      {linkDocs && <Link href={linkDocs} text={MSG.linkTextDocs} />}
       <Link href={linkGithub} text={MSG.linkTextGithub} />
     </div>
   </div>
@@ -133,33 +166,74 @@ const DeveloperTools = () => {
                 </button>
               ))}
           </div>
-          <div className={styles.activeContent}>
-            <Paragraph text={MSG[`bodyTopic${activeItem}`]} />
+          <div className={styles.body}>
+            <Paragraph text={MSG.body} />
           </div>
         </div>
       </div>
-      <div className={styles.gridRow}>
-        <GridItem
-          body={MSG.bodyGridItem0}
-          iconName="logomark_colonyjs"
-          linkDocs={DOCS_COLONY_JS_GET_STARTED}
-          linkGithub={COLONY_GITHUB_COLONYJS}
-          title="colonyJS"
-        />
-        <GridItem
-          body={MSG.bodyGridItem1}
-          iconName="logomark_colonystarter"
-          linkDocs={DOCS_COLONY_STARTER}
-          linkGithub={COLONY_GITHUB_COLONYSTARTER}
-          title="colonyStarter"
-        />
-        <GridItem
-          body={MSG.bodyGridItem2}
-          iconName="logomark_colonynetwork"
-          linkDocs={DOCS_COLONY_NETWORK}
-          linkGithub={COLONY_GITHUB_COLONYNETWORK}
-          title="colonyNetwork"
-        />
+      <div className={styles.gridWrapper}>
+        <div className={styles.gridRow}>
+          {activeItem === 0 ? (
+            <>
+              <GridItem
+                body={MSG_CORE.bodyColonyJS}
+                iconName="logomark_colonyjs"
+                linkDocs={DOCS_COLONY_JS_GET_STARTED}
+                linkGithub={COLONY_GITHUB_COLONYJS}
+                title="colonyJS"
+              />
+              <GridItem
+                body={MSG_CORE.bodyColonyStarter}
+                iconName="logomark_colonystarter"
+                linkDocs={DOCS_COLONY_STARTER}
+                linkGithub={COLONY_GITHUB_COLONYSTARTER}
+                title="colonyStarter"
+              />
+              <GridItem
+                body={MSG_CORE.bodyColonyNetwork}
+                iconName="logomark_colonynetwork"
+                linkDocs={DOCS_COLONY_NETWORK}
+                linkGithub={COLONY_GITHUB_COLONYNETWORK}
+                title="colonyNetwork"
+              />
+            </>
+          ) : (
+            <>
+              <GridItem
+                body={MSG_COMMUNITY.bodyPinion}
+                iconName="logomark_pinion"
+                linkGithub={COLONY_GITHUB_PINION}
+                title="pinion"
+              />
+              <GridItem
+                body={MSG_COMMUNITY.bodyPurser}
+                iconName="logomark_purser"
+                linkDocs={DOCS_COLONY_PURSER}
+                linkGithub={COLONY_GITHUB_PURSER}
+                title="purser"
+              />
+              <GridItem
+                body={MSG_COMMUNITY.bodyTailor}
+                iconName="logomark_tailor"
+                linkDocs={DOCS_COLONY_TAILOR}
+                linkGithub={COLONY_GITHUB_TAILOR}
+                title="tailor"
+              />
+              <GridItem
+                body={MSG_COMMUNITY.bodyTrufflePig}
+                iconName="logomark_trufflePig"
+                linkGithub={COLONY_GITHUB_TRUFFLEPIG}
+                title="trufflePig"
+              />
+              <GridItem
+                body={MSG_COMMUNITY.bodySolidityCoverage}
+                iconName="logomark_solidityCoverage"
+                linkGithub={GITHUB_SOLIDITY_COVERAGE}
+                title="solidity-coverage"
+              />
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
