2c80ccada5320a793a2b08ad69f6d25195535776
Curtis Olson <curtis@colony.io>
2019-08-09 17:46:24 -0500

    Use new images for slider graphic

--- src/modules/pages/components/Website/HomePage/Vision/assets/index.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-
-export { default as Vision0 } from './Vision0.jsx';
-export { default as Vision1 } from './Vision1.jsx';
-export { default as Vision2 } from './Vision2.jsx';
-export { default as Vision3 } from './Vision3.jsx';
-export { default as Vision4 } from './Vision4.jsx';
-export { default as Vision5 } from './Vision5.jsx';
