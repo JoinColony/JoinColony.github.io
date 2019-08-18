1332e2acabf6ab16009dfc90c6c781ffcc86941f
Curtis Olson <curtis@colony.io>
2019-08-18 14:12:48 -0500

    Responsive kick ass section for product platform page

--- src/modules/core/components/GridItem/GridItem.jsx
+++ src/modules/core/components/GridItem/GridItem.jsx
@@ -6,6 +6,7 @@ import React from 'react';
 
 import Heading from '~core/Heading';
 import Image from '~core/Image';
+import Link from '~core/Link';
 import Paragraph from '~core/Paragraph';
 
 import styles from './GridItem.module.css';
@@ -13,26 +14,38 @@ import styles from './GridItem.module.css';
 type Props = {|
   body: MessageDescriptor,
   image: string,
+  links?: Array<{ href: string, text: MessageDescriptor }>,
   title: MessageDescriptor,
 |};
 
 const displayName = 'GridItem';
 
-const GridItem = ({ body, image, title }: Props) => (
+const GridItem = ({ body, image, links = [], title }: Props) => (
   <div className={styles.main}>
     <div>
       <Image alt={title} className={styles.image} src={image} />
     </div>
-    <div>
-      <Heading
-        appearance={{
-          theme: 'dark',
-          size: 'mediumLarge',
-          weight: 'medium',
-        }}
-        text={title}
-      />
-      <Paragraph text={body} />
+    <div className={styles.content}>
+      <div className={styles.contentInner}>
+        <div className={styles.heading}>
+          <Heading
+            appearance={{
+              theme: 'dark',
+              size: 'mediumLarge',
+              weight: 'medium',
+            }}
+            text={title}
+          />
+        </div>
+        <div className={styles.body}>
+          <Paragraph text={body} />
+        </div>
+      </div>
+      {links.map(({ href, text }) => (
+        <div className={styles.links}>
+          <Link className={styles.itemLink} href={href} text={text} />
+        </div>
+      ))}
     </div>
   </div>
 );
