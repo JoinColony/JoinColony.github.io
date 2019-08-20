1644e852076e9ac18ac3dfc1daee9c18da177d37
Curtis Olson <curtis@colony.io>
2019-08-20 12:19:52 -0500

    Add form logic

--- src/modules/layouts/components/WebsiteLayout/Footer/Footer.jsx
+++ src/modules/layouts/components/WebsiteLayout/Footer/Footer.jsx
@@ -1,10 +1,14 @@
 /* @flow */
 
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { defineMessages } from 'react-intl';
+import { withPrefix } from 'gatsby';
 
+import Button from '~core/Button';
 import Heading from '~core/Heading';
+import Input from '~core/Input';
 import Icon from '~core/Icon';
+import Image from '~core/Image';
 import Link from '~core/Link';
 import MetaNav from '~parts/MetaNav';
 import SocialNav from '~parts/SocialNav';
@@ -15,6 +19,14 @@ import Menus from './Menus';
 import styles from './Footer.module.css';
 
 const MSG = defineMessages({
+  placeholder: {
+    id: 'layouts.WebsiteLayout.Footer.placeholder',
+    defaultMessage: 'Email, please',
+  },
+  submit: {
+    id: 'layouts.WebsiteLayout.Footer.submit',
+    defaultMessage: 'Submit',
+  },
   textSubscribe: {
     id: 'layouts.WebsiteLayout.Footer.textSubscribe',
     defaultMessage: 'Subscribe to our newsletter',
@@ -23,48 +35,97 @@ const MSG = defineMessages({
 
 const displayName = 'layouts.WebsiteLayout.Footer';
 
-const Footer = () => (
-  <>
-    <div className={styles.divider} />
-    <div className={styles.main}>
-      <div className={styles.row}>
-        <div className={styles.contentContainer}>
-          <div>
-            <div className={styles.logoContainer}>
-              <Link href={PAGE_INDEX}>
-                <Icon
-                  className={styles.logo}
-                  name="colony_logomark"
-                  title="Colony"
+const Footer = () => {
+  const [email, setEmail] = useState('');
+
+  const handleChange = useCallback(
+    (event: SyntheticInputEvent<HTMLInputElement>) =>
+      setEmail(event.currentTarget.value),
+    [],
+  );
+
+  const handleSubmit = useCallback(
+    (event: SyntheticEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const formEndpoint =
+        // eslint-disable-next-line max-len
+        '//colony.us9.list-manage.com/subscribe/post?u=f4d1f0850fe4aaa32124d89ca&id=fef68720b5';
+      // eslint-disable-next-line no-undef
+      const formData = new FormData();
+      formData.append('subscribe', email);
+      // eslint-disable-next-line no-undef
+      fetch(formEndpoint, {
+        method: 'POST',
+        body: formData,
+        headers: { 'content-type': 'multipart/form-data' },
+      })
+        .then()
+        .then(() => {
+          setEmail('');
+        });
+    },
+    [email],
+  );
+
+  return (
+    <>
+      <div className={styles.divider} />
+      <div className={styles.main}>
+        <div className={styles.row}>
+          <div className={styles.contentContainer}>
+            <div>
+              <div className={styles.logoContainer}>
+                <Link href={PAGE_INDEX}>
+                  <Icon
+                    className={styles.logo}
+                    name="colony_logomark"
+                    title="Colony"
+                  />
+                </Link>
+              </div>
+              <Heading
+                appearance={{
+                  size: 'mediumLarge',
+                  theme: 'grey',
+                  weight: 'thin',
+                }}
+                text={MSG.textSubscribe}
+              />
+              <form className={styles.form} onSubmit={handleSubmit}>
+                <Input
+                  className={styles.input}
+                  id={`${displayName}email`}
+                  onChange={handleChange}
+                  placeholder={MSG.placeholder}
+                  type="email"
+                  value={email}
                 />
-              </Link>
+                <Button
+                  appearance={{ theme: 'reset' }}
+                  className={styles.button}
+                  type="submit"
+                >
+                  <Image alt={MSG.submit} src={withPrefix('/img/submit.png')} />
+                </Button>
+              </form>
             </div>
-            <Heading
-              appearance={{
-                size: 'mediumLarge',
-                theme: 'grey',
-                weight: 'thin',
-              }}
-              text={MSG.textSubscribe}
-            />
-            {/* @TODO: input here, make heading above a label */}
+            <div className={styles.menu}>
+              <Menus />
+            </div>
           </div>
-          <div className={styles.menu}>
-            <Menus />
-          </div>
-        </div>
-        <div className={styles.metaRow}>
-          <div className={styles.metaNavContainer}>
-            <MetaNav />
-          </div>
-          <div className={styles.socialNavContainer}>
-            <SocialNav appearance={{ theme: 'light' }} />
+          <div className={styles.metaRow}>
+            <div className={styles.metaNavContainer}>
+              <MetaNav />
+            </div>
+            <div className={styles.socialNavContainer}>
+              <SocialNav appearance={{ theme: 'light' }} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 Footer.displayName = displayName;
 
