7224c310c0bd35a919615f483e0e15531de22c6a
Curtis Olson <curtis@colony.io>
2019-08-20 12:19:52 -0500

    Add form logic

--- src/modules/parts/components/NewsletterCta/NewsletterCta.jsx
+++ src/modules/parts/components/NewsletterCta/NewsletterCta.jsx
@@ -31,7 +31,18 @@ const displayName = 'parts.NewsletterCta';
 
 const NewsletterCta = ({ id }: Props) => {
   const handleSubmit = useCallback((value: string) => {
-    console.log(value);
+    const formEndpoint =
+      // eslint-disable-next-line max-len
+      '//colony.us9.list-manage.com/subscribe/post?u=f4d1f0850fe4aaa32124d89ca&id=fef68720b5';
+    // eslint-disable-next-line no-undef
+    const formData = new FormData();
+    formData.append('subscribe', value);
+    // eslint-disable-next-line no-undef
+    fetch(formEndpoint, {
+      method: 'POST',
+      body: formData,
+      headers: { 'content-type': 'multipart/form-data' },
+    });
   }, []);
 
   return (
