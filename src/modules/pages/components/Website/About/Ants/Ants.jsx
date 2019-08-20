18561ca6a318f7f960f7c948c79d4323ba1ac135
Curtis Olson <curtis@colony.io>
2019-08-20 11:28:33 -0500

    Remove comma

--- src/modules/pages/components/Website/About/Ants/Ants.jsx
+++ src/modules/pages/components/Website/About/Ants/Ants.jsx
@@ -15,7 +15,7 @@ const MSG = defineMessages({
   },
   body: {
     id: 'pages.Website.About.Ants.body',
-    defaultMessage: `Colony is inspired by complex adaptive systems, like
+    defaultMessage: `Colony is inspired by complex adaptive systems like
       ant colonies: dynamic networks of agents which react and adapt to
       stimuli from each other and their environment. From simple rules
       emerges collective behaviour and self-organization of the system
