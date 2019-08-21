daa5b895ee54380a0b5e95adef8267cb1ac14e39
Alex <git@area.io>
2019-08-21 18:16:10 +0100

    Actually finish setting up ssh keys for deploy

--- scripts/authGoogle.sh
+++ scripts/authGoogle.sh
@@ -5,4 +5,6 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
-cat gcloud-service-key.json | jq -r .private_key > ~/.ssh/google_compute_engine
+cat ~/gcloud-service-key.json | jq -r .private_key > ~/.ssh/google_compute_engine
+chmod 0600 ~/.ssh/google_compute_engine
+ssh-keygen -y -f ~/.ssh/google_compute_engine > ~/.ssh/google_compute_engine.pub
