dbcae3b851a69fce883ae97f5074c89a936599c9
Curtis Olson <curtis@colony.io>
2020-06-23 17:51:05 -0500

    Use discord webhook for contact form

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,4 +1,5 @@
 /* @flow */
 
+export { default as useDiscordWebhook } from './useDiscordWebhook';
 export { default as useElementHeight } from './useElementHeight';
 export { default as useHubspotForm } from './useHubspotForm';
